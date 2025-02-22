19b2e98f56358ea753a47e46cf997fd28e05c482
MengYX <mengyx@stoneflicker.com>
2025-02-22 21:11:39 +0800

    reproduce for https://github.com/ent/ent/issues/4337

---
+++ ent/schema/user.go
@@ -0,0 +1,31 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/dialect"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema/field"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.Time("created_at").
+			Default(time.Now).
+			Annotations(entsql.DefaultExprs(map[string]string{
+				dialect.Postgres: "NOW()",
+			})),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return nil
+}
